d7a7aa3abdf3b6d8b3b509c3c8f1401e4ed3d81c
RaymondRemble <remble96@tutanota.com>
2018-07-13 01:37:16 +0800

    Upgrade

--- src/api/v1.0/getUpTime/getUpTime.ctrl.js
+++ src/api/v1.0/getUpTime/getUpTime.ctrl.js
@@ -3,5 +3,5 @@
 exports.balance = async (ctx) => {
   const { userId } = ctx.params;
 
-  ctx.body = {'User':'user1', 'UpTime':'130m', 'Status':'Online'};
+  ctx.body = {'user':'user1', 'onlineDur':'130m', 'offlineTimes': 5 };
 }
