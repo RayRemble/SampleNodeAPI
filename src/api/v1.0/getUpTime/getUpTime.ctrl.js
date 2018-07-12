aad546e646c8bfe38fbe20f571840dd99057df7a
RaymondRemble <remble96@tutanota.com>
2018-07-13 02:29:21 +0800

    Upgrade third

--- src/api/v1.0/getUpTime/getUpTime.ctrl.js
+++ src/api/v1.0/getUpTime/getUpTime.ctrl.js
@@ -2,6 +2,13 @@
 
 exports.balance = async (ctx) => {
   const { userId } = ctx.params;
-
-  ctx.body = {'user':'user1', 'onlineDur':'130', 'offlineTimes': '5', 'currentStatus':'online' };
+  if(userId == "user1"){
+    ctx.body = {'user':'user1', 'onlineDur':'130', 'offlineTimes': '5', 'currentStatus':'online' };
+  }
+  else if(userId == "user2"){
+    ctx.body = {'user':'user2', 'onlineDur':'190', 'offlineTimes': '9', 'currentStatus':'offline' };
+  }
+  else if(userId == "user3"){
+    ctx.body = {'user':'user3', 'onlineDur':'430', 'offlineTimes': '2', 'currentStatus':'online' };
+  }
 }
